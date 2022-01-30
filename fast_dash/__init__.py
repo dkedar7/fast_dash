596f71fa7d323e2cff53ccd59eb62326e033e425
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2022-01-29 20:33:38 -0600

    Tox update

--- fast_dash/__init__.py
+++ fast_dash/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for Fast Dash."""
 
 __author__ = """Kedar Dabhadkar"""
-__email__ = 'kdabhadk@gmail.com'
-__version__ = '0.1.0'
+__email__ = "kdabhadk@gmail.com"
+__version__ = "0.1.0"
