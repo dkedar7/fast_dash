813ea3cab9433aafbfa78d7827ae428cc8e18652
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2022-03-19 14:46:54 -0500

    Fix: Attempt running headless selenium for Github actions

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,4 +4,5 @@ from selenium.webdriver.chrome.options import Options
 def pytest_setup_options():
     options = Options()
     options.add_argument("--disable-gpu")
+    options.add_argument('--headless')
     return options
