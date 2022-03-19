f73bb1780637824a19af57cbbdccedc4db3c9d1b
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2022-03-19 13:38:08 -0500

    Add first pytest

--- tests/test_fast_dash.py
+++ tests/test_fast_dash.py
@@ -2,21 +2,33 @@
 """Tests for `fast_dash` package."""
 # pylint: disable=redefined-outer-name
 
-import pytest
+from fast_dash import FastDash, Fastify
+from fast_dash.Components import Text, Image, Upload, dbc
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+## Define callback functions
+def simple_text_to_text_function(input_text):
+    return input_text
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-    del response
+def text_to_text_function(choice):
+    processed_text1 = f"Output of {choice}."
+    return processed_text1, choice
+
+
+def test_bsly001_falsy_child(dash_duo):
+    # 3. define your app inside the test function
+    app = FastDash(
+        callback_fn=simple_text_to_text_function,
+        inputs=Text,
+        outputs=Text,
+        title="App title",
+    ).app
+
+    dash_duo.start_server(app)
+    dash_duo.wait_for_text_to_equal("#app_title", "App title", timeout=4)
+
+    assert dash_duo.find_element("#app_title").text == "App title"
+    assert dash_duo.get_logs() == [], "browser console should contain no error"
+
+    dash_duo.percy_snapshot("bsly001-layout")
