6582970d7a6236cc2f889001c7958dcbfbb24a64
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2022-03-21 20:23:24 -0500

    Modify examples

--- examples/Neural style transfer/app.py
+++ examples/Neural style transfer/app.py
@@ -1,5 +1,5 @@
 from fast_dash import FastDash, Fastify
-from fast_dash.Components import Upload, Image
+from fast_dash.Components import UploadImage, Image, html
 from dash import dcc
 
 from utils import stylize_image, map_style_model_path
@@ -11,15 +11,18 @@ def apply_style(image, style):
     "Callback to apply style to image"
     image_type, image_content = image.split(',')
     styled_image = stylize_image(image_content, style)
-    return image, styled_image
+    return styled_image
 
 app = FastDash(callback_fn=apply_style, 
-                inputs=[Upload, dropdown], 
-                outputs=[Image, Image], 
+                inputs=[UploadImage, dropdown], 
+                outputs=Image, 
                 title='Neural Style Transfer',
                 title_image_path='https://raw.githubusercontent.com/dkedar7/fast_dash/main/examples/Neural%20style%20transfer/assets/icon.png',
                 subheader="Apply styles from well-known pieces of art to your own photos",
-                theme='SKETCHY')
+                github_url='https://github.com/dkedar7/fast_dash/',
+                linkedin_url='https://linkedin.com/in/dkedar7/',
+                twitter_url='https://twitter.com/dkedar7/',
+                theme='JOURNAL')
                 
 if __name__ == '__main__':
     app.run()
