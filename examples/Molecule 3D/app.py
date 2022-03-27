be827871372044083b548f6103cabb308ca29afd
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2022-03-26 21:00:05 -0500

    Add example: Dash bio molecule 3 viewer

---
+++ examples/Molecule 3D/app.py
@@ -0,0 +1,17 @@
+from fast_dash import FastDash, Fastify
+from fast_dash.Components import Text, dcc, html
+from utils import render_molecule, data_info
+
+mol_dropdown = Fastify(dcc.Dropdown, 'value', options=[{'label':k, 'value':k} for k in data_info])
+output_div = Fastify(html.Div, 'children')
+
+app = FastDash(callback_fn=render_molecule, 
+                inputs=mol_dropdown, 
+                outputs=output_div, 
+                title='Molecule 3D Viewer',
+                subheader="Molecule3D is a visualizer that allows you to view biomolecules in multiple representations: sticks, spheres, and cartoons.",
+                title_image_path="https://raw.githubusercontent.com/dkedar7/fast_dash/examples/examples/Molecule%203D/assets/molecule.png",
+                theme="Pulse")
+
+if __name__ == '__main__':
+    app.run()
