2eed0db58f3e4613132f2f8ec5b2f6607cbc3609
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2022-04-02 09:35:56 -0500

    Edit example: Theme and title image in translation

--- examples/Translate to multiple languages/app.py
+++ examples/Translate to multiple languages/app.py
@@ -11,7 +11,13 @@ def translate_from_english(english_sentence, select_language):
     translation = translate(english_sentence, select_language)
     return translation
 
-app = FastDash(callback_fn=translate_from_english, inputs=[Text, dropdown], outputs=Text, title='Translate from English')
+app = FastDash(callback_fn=translate_from_english, 
+                inputs=[Text, dropdown], 
+                outputs=Text, 
+                title='Translate to Multiple Languages', 
+                subheader='Enter English text and choose target language to translate!',
+                title_image_path='https://raw.githubusercontent.com/dkedar7/fast_dash/examples/examples/Translate%20to%20multiple%20languages/assets/icon_translation.png',
+                theme='YETI')
 
 if __name__ == '__main__':
     app.run()
