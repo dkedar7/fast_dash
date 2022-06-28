5a27f213f8e7ee219927fcf150ee9061d7fd6c1d
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2022-06-28 06:41:16 -0500

    Bring all imports to __init__ for easy access

--- fast_dash/__init__.py
+++ fast_dash/__init__.py
@@ -4,7 +4,36 @@ __author__ = """Kedar Dabhadkar"""
 __email__ = "kedar@fastdash.app"
 __version__ = "0.1.6"
 
-from fast_dash.Components import Fastify
-from fast_dash.fast_dash import FastDash
+from fast_dash.Components import (
+    Fastify,
+    Text,
+    TextArea,
+    Slider,
+    Upload,
+    acknowledge_image_component,
+    UploadImage,
+    Image,
+    Graph,
+    dcc,
+    dbc,
+    html
+)
+from fast_dash.fast_dash import FastDash, fastdash
 
-__all__ = ["FastDash", "Fastify"]
+__all__ = [
+    "FastDash",
+    "fastdash",
+    "Fastify",
+    "Text",
+    "TextArea",
+    "Slider",
+    "Upload",
+    "acknowledge_image_component",
+    "UploadImage",
+    "Image",
+    "Graph",
+    "dcc",
+    "dbc",
+    "html"
+]
+
