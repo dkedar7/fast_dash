a9b5fcee2fe1b72951703062456120c7cddd69c9
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2023-07-02 17:05:33 -0400

    [Tests] Change screen size for headless pytests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,6 @@ def pytest_setup_options():
     options = Options()
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
+    options.add_argument("--window-size=1280x1024")
     options.add_argument("--disable-pytest-warnings")
     return options
