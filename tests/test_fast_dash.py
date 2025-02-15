d0137074fd5deea5494fc2f3f50331c0116d8598
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2025-02-15 17:10:13 -0500

    [Tests] Replica the test fix for fd05

--- tests/test_fast_dash.py
+++ tests/test_fast_dash.py
@@ -128,8 +128,10 @@ def test_fdfd005_multiple_outputs(dash_duo):
     dash_duo.wait_for_text_to_equal("#output-2", "Sample text", timeout=4)
 
     # Click clear
-    dash_duo.wait_for_element_by_css_selector("#reset_inputs", timeout=4)
-    dash_duo.multiple_click("#reset_inputs", 1)
+    # Click clear
+    wait = WebDriverWait(dash_duo.driver, 10)
+    reset_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#reset_inputs")))
+    dash_duo.driver.execute_script("document.querySelector('#reset_inputs').click()")
     dash_duo.wait_for_text_to_equal("#output-1", "", timeout=4)
     dash_duo.wait_for_text_to_equal("#output-2", "", timeout=4)
 
