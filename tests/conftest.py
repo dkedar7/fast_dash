7c9e3a925180763c5912bff88b208525c63fc36b
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2025-06-26 21:17:25 -0700

    Comment out headless option in pytest setup for better debugging visibility

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 def pytest_setup_options():
     options = Options()
     options.add_argument("--disable-gpu")
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     options.add_argument("--window-size=1280x1024")
     options.add_argument("--disable-pytest-warnings")
     return options
